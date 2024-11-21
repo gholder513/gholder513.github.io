bf5e29596bc2dda905a464cd2c8b6c33ea24d036
gholder513 <gholder513@gmail.com>
2024-11-21 16:03:59 -0500

    Changes to package.json

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/gholder513.github.io/', // Adjust based on your repository name
+  base: '/', 
   plugins: [react()],
 });
